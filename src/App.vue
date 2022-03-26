b42484fd0e34caed06afc1e64cf74a54be09df91
Sergey <forsubscriptions2019@gmail.com>
2022-03-26 10:17:23 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>App</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
