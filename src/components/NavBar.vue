0588320fc84e6ae919b37f5f2792b7342d802a3d
Sergey <forsubscriptions2019@gmail.com>
2022-04-08 20:08:34 +0300

    Started working on signin form

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,11 +3,11 @@
     <div class="container">
       <router-link :to="{ name: 'home' }" class="logo">Getsy</router-link>
       <div class="menu">
-        <router-link :to="{ name: 'services' }" class="link"
-          >Services</router-link
-        >
         <router-link :to="{ name: 'products' }" class="link"
           >Products</router-link
+        >
+        <router-link :to="{ name: 'services' }" class="link"
+          >Services</router-link
         >
       </div>
       <div class="user">
@@ -15,22 +15,44 @@
         <Button
           label="Log in"
           class="p-button-sm p-button-outlined p-button-text"
+          @click="isDisplayed = !isDisplayed"
         />
       </div>
     </div>
+    <Dialog v-model:visible="isDisplayed">
+      <template #header>
+        <h3>Header</h3>
+      </template>
+
+      <Login v-if="hasAccount" />
+      <SignUp v-if="!hasAccount" />
+
+      <button class="button" @click="hasAccount = !hasAccount">
+        {{ hasAccount ? 'Already has an account?' : 'Log In' }}
+      </button>
+    </Dialog>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import Dialog from 'primevue/dialog';
 import Button from 'primevue/button';
+import Login from '../components/auth/Login.vue';
+import SignUp from '../components/auth/SignUp.vue';
 
 export default defineComponent({
   components: {
     Button,
+    Dialog,
+    Login,
+    SignUp,
   },
   setup() {
-    return {};
+    const isDisplayed = ref(true);
+    const hasAccount = ref(true);
+
+    return { isDisplayed, hasAccount };
   },
 });
 </script>
@@ -40,6 +62,7 @@ export default defineComponent({
 
 $logoColor: var(--orange-500);
 $borderColor: var(--teal-50);
+$activeColor: var(--teal-500);
 .navbar {
   border-bottom: 1px solid $borderColor;
   background-color: #fff;
@@ -62,11 +85,17 @@ $borderColor: var(--teal-50);
   margin: 0 20px;
   font-size: 15px;
 }
+.button {
+  background-color: transparent;
+  border: none;
+  color: $activeLinkColor;
+  padding: 0;
+}
 .router-link-active {
   text-decoration: underline;
 }
 .logo {
-  font-weight: 800;
+  font-weight: 600;
   font-size: 24px;
   color: $logoColor;
   text-decoration: none;
---
+++ src/components/auth/Login.vue
@@ -0,0 +1,74 @@
+<template>
+  <form class="container" @submit.prevent="submitForm">
+    <div class="field">
+      <label for="username2">Username</label>
+      <InputText
+        id="email"
+        type="text"
+        aria-describedby="username2-help"
+        class="p-inputtext-sm input"
+        autocomplete="off"
+        v-model.lazy="formData.email"
+      />
+      <small id="email-help" class="p-error"> Username is required </small>
+    </div>
+    <div class="field">
+      <label for="username2">Password</label>
+      <InputText
+        id="password"
+        type="password"
+        aria-describedby="username2-help"
+        class="p-inputtext-sm"
+        v-model.lazy="formData.password"
+      />
+      <small id="password-help" class="p-error"> Password is required </small>
+    </div>
+    <Button
+      type="submit"
+      label="Log in"
+      class="p-button-sm p-button-outlined p-button-text button"
+    />
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import { AuthType } from '../../types/auth.interface';
+
+export default defineComponent({
+  components: { InputText, Button },
+  setup() {
+    const formData = <AuthType>reactive({
+      email: '',
+      password: '',
+    });
+
+    const submitForm = () => {
+      console.log(formData);
+      formData.email = '';
+      formData.password = '';
+    };
+    return { formData, submitForm };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  min-width: 300px;
+  display: flex;
+  flex-direction: column;
+}
+.button {
+  margin-left: auto;
+}
+
+.field {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+</style>
