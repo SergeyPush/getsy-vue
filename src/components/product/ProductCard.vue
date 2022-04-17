91678ba69b2ebabd007aed772725a9011a6a8afe
Sergey <forsubscriptions2019@gmail.com>
2022-04-17 19:32:23 +0300

    Updated product card

--- src/components/product/ProductCard.vue
+++ src/components/product/ProductCard.vue
@@ -1,12 +1,28 @@
 <template>
   <router-link :to="{ name: 'product', params: { id: product.id } }">
-    <Card class="card">
-      <template #header> </template>
-      <template #title> {{ product.title }} </template>
-      <template #content>
-        <p class="text" v-html="product.description" />
-      </template>
-    </Card>
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img
+            src="https://bulma.io/images/placeholders/1280x960.png"
+            alt="Placeholder image"
+          />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{ product.title }}</p>
+          </div>
+        </div>
+        <div class="content">
+          <p class="text">
+            {{ product.description }}
+          </p>
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+      </div>
+    </div>
   </router-link>
 </template>
 
